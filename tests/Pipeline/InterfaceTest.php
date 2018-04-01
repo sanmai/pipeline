dd918abe2dc00a78a2545378f66230f81c4252e1
Alexey Kopytko <alexey@kopytko.com>
2018-04-02 00:08:12 +0900

    InterfaceTest: confirms that indeed the standard pipeline conforms to every interface

---
+++ tests/Pipeline/InterfaceTest.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Copyright 2017, 2018 Alexey Kopytko <alexey@kopytko.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Pipeline;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Pipeline\Standard
+ * @covers \Pipeline\Principal
+ */
+class InterfaceTest extends TestCase
+{
+    private function takesStandardInterface(Interfaces\StandardPipeline $pipeline)
+    {
+        return $pipeline;
+    }
+
+    public function testStandardInterface()
+    {
+        $pipeline = new Standard();
+
+        $this->assertInstanceOf(Interfaces\StandardPipeline::class, $this->takesStandardInterface($pipeline));
+    }
+
+    private function takesPrincipalInterface(Interfaces\Pipeline $pipeline)
+    {
+        return $pipeline;
+    }
+
+    public function testPrincipalInterface()
+    {
+        $pipeline = new Standard();
+
+        $this->assertInstanceOf(Interfaces\Pipeline::class, $this->takesPrincipalInterface($pipeline));
+    }
+}
