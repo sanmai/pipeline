55376dca794d95d0058ad996d6b135978ca8167a
Alexey Kopytko <alexey@kopytko.com>
2018-10-21 15:00:06 +0900

    Test that reduce() works with arrays eagerly

--- tests/EagerWithArraysTest.php
+++ tests/EagerWithArraysTest.php
@@ -21,6 +21,7 @@ namespace Tests\Pipeline;
 
 use PHPUnit\Framework\TestCase;
 use Pipeline\Principal;
+use Pipeline\Standard;
 use function Pipeline\fromArray;
 use function Pipeline\take;
 
@@ -29,25 +30,41 @@ use function Pipeline\take;
  */
 class EagerWithArraysTest extends TestCase
 {
-    public function testEagerArrayFilter()
+    /**
+     * @dataProvider specimens
+     */
+    public function testEagerArrayFilter(Standard $pipeline)
     {
         $reflectionClass = new \ReflectionClass(Principal::class);
         $reflectionProperty = $reflectionClass->getProperty('pipeline');
         $reflectionProperty->setAccessible(true);
 
-        foreach ([
-            take([0, 0, 1, 2, 3]),
-            fromArray([0, 0, 1, 2, 3]),
-        ] as $pipeline) {
-            $pipeline->filter();
-            // At this point $pipeline contains exactly [1, 2, 3]
+        $pipeline->filter();
+        // At this point $pipeline should contain exactly [1, 2, 3]
 
-            $this->assertSame([2 => 1, 2, 3], $reflectionProperty->getValue($pipeline));
+        $this->assertSame([2 => 1, 2, 3], $reflectionProperty->getValue($pipeline));
 
-            $this->assertSame([1, 2, 3], $pipeline->toArray());
+        $this->assertSame([1, 2, 3], $pipeline->toArray());
 
-            // This does nothing more
-            $this->assertSame([1, 2, 3], $pipeline->filter()->toArray());
-        }
+        // This does nothing more
+        $this->assertSame([1, 2, 3], $pipeline->filter()->toArray());
+    }
+
+    /**
+     * @dataProvider specimens
+     */
+    public function testEagerArrayReduce(Standard $pipeline)
+    {
+        $this->assertSame(6, $pipeline->reduce());
+
+        // Second reduce over the same pipeline is impossible with an underlying generator
+        // But should be possible with an array
+        $this->assertSame(6, $pipeline->reduce());
+    }
+
+    public static function specimens(): \Generator
+    {
+        yield 'take' => [take([0, 0, 1, 2, 3])];
+        yield 'fromArray' => [fromArray([0, 0, 1, 2, 3])];
     }
 }
