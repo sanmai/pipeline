f597fde65677c5e1f9006e337dac560fd5e11a81
Alexey Kopytko <alexey@kopytko.com>
2025-05-14 07:55:46 +0900

    tests/ValuesTest.php

---
+++ tests/ValuesTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Copyright 2017, 2018 Alexey Kopytko <alexey@kopytko.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Pipeline;
+
+use ArrayIterator;
+use IteratorIterator;
+use PHPUnit\Framework\TestCase;
+use Pipeline\Standard;
+
+use function array_flip;
+use function array_reverse;
+use function call_user_func;
+use function chr;
+use function count;
+use function Pipeline\fromArray;
+use function Pipeline\map;
+use function Pipeline\take;
+use function shuffle;
+
+/**
+ * @covers \Pipeline\Standard
+ *
+ * @internal
+ */
+final class ValuesTest extends TestCase
+{
+    public function testValuesArray(): void
+    {
+        $values = fromArray(['a' => 1, 'b' => 2, 'c' => 3])
+            ->values()
+            ->toArray();
+        $this->assertSame([1, 2, 3], $values);
+    }
+
+    public function testValuesIterator(): void
+    {
+        $values = map(static function () {
+            yield 5 => 'a';
+            yield 6 => 'b';
+            yield 5 => 'c';
+            yield 6 => 'd';
+        })->values()->toArray();
+
+        $this->assertSame(['a', 'b', 'c', 'd'], $values);
+    }
+
+    public function testValuesIteratorPreserveKeys(): void
+    {
+        $values = map(static function () {
+            yield 'x';
+            yield 'y';
+        })->map(static function ($value) {
+            yield 'c' => $value;
+            yield 'd' => $value;
+        })->values()->toArray(preserve_keys: true);
+
+        $this->assertSame(['x', 'x', 'y', 'y'], $values);
+    }
+}
