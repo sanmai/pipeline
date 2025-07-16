23c568e12a17dfd14a46153afb5bb67cdfe0d388
Alexey Kopytko <alexey@kopytko.com>
2025-07-16 09:28:16 +0900

    improve

--- src/Standard.php
+++ src/Standard.php
@@ -776,35 +776,49 @@ class Standard implements IteratorAggregate, Countable
             return $this;
         }
 
-        $this->pipeline = self::makeNonRewindable($this->pipeline);
-
-        if ($offset < 0) {
-            // If offset is negative, the sequence will start that far from the end of the array.
-            $this->pipeline = self::tail($this->pipeline, -$offset);
-        }
-
-        if ($offset > 0) {
-            // If offset is non-negative, the sequence will start at that offset in the array.
-            $this->pipeline = self::skip($this->pipeline, $offset);
-        }
-
-        if ($length < 0) {
-            // If length is given and is negative then the sequence will stop that many elements from the end of the array.
-            $this->pipeline = self::head($this->pipeline, -$length);
-        }
-
-        if ($length > 0) {
-            // If length is given and is positive, then the sequence will have up to that many elements in it.
-            $this->pipeline = self::take($this->pipeline, $length);
-        }
+        // Transform the pipeline through offset and length operations
+        $this->pipeline = self::applySliceTransformations(
+            self::makeNonRewindable($this->pipeline),
+            $offset,
+            $length
+        );
 
         return $this;
     }
 
     /**
+     * Applies slice transformations based on offset and length.
+     * Each transformation flows naturally into the next.
+     */
+    private static function applySliceTransformations(Generator $stream, int $offset, ?int $length): Iterator
+    {
+        if ($offset > 0) {
+            // If offset is negative, the sequence will start that far from the end of the array.
+            $stream = self::skip($stream, $offset);
+        }
+
+        if ($offset < 0) {
+            // If offset is non-negative, the sequence will start at that offset in the array.
+            $stream = self::tail($stream, -$offset);
+        }
+
+        if ($length < 0) {
+            // If length is given and is negative then the sequence will stop that many elements from the end of the array.
+            $stream = self::head($stream, -$length);
+        }
+
+        if ($length > 0) {
+            // If length is given and is positive, then the sequence will have up to that many elements in it.
+            $stream = self::take($stream, $length);
+        }
+
+        return $stream;
+    }
+
+    /**
      * @psalm-param positive-int $skip
      */
-    private static function skip(Iterator $input, int $skip): Generator
+    private static function skip(Iterator $input, int $skip): Iterator
     {
         // Consume until seen enough.
         foreach ($input as $_) {
@@ -826,7 +840,7 @@ class Standard implements IteratorAggregate, Countable
     /**
      * @psalm-param positive-int $take
      */
-    private static function take(Generator $input, int $take): Generator
+    private static function take(Iterator $input, int $take): Iterator
     {
         while ($input->valid()) {
             yield $input->key() => $input->current();
@@ -840,7 +854,7 @@ class Standard implements IteratorAggregate, Countable
         }
     }
 
-    private static function tail(iterable $input, int $length): Generator
+    private static function tail(iterable $input, int $length): Iterator
     {
         $buffer = [];
 
@@ -865,7 +879,7 @@ class Standard implements IteratorAggregate, Countable
     /**
      * Allocates a buffer of $length, and reads records into it, proceeding with FIFO when buffer is full.
      */
-    private static function head(iterable $input, int $length): Generator
+    private static function head(iterable $input, int $length): Iterator
     {
         $buffer = [];
 
