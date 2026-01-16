df265e259673e972601b0f303de11e34333439c2
Alexey Kopytko <alexey@kopytko.com>
2026-01-16 20:20:44 +0900

    Refactor optimization for internal callables

--- tests/SelectTest.php
+++ tests/SelectTest.php
@@ -21,15 +21,13 @@ declare(strict_types=1);
 namespace Tests\Pipeline;
 
 use ArrayIterator;
+use PHPUnit\Framework\TestCase;
 use Pipeline\Standard;
 use PHPUnit\Framework\Attributes\DataProvider;
-use SplQueue;
 
 use function iterator_to_array;
-use function Pipeline\fromArray;
 use function Pipeline\fromValues;
 use function Pipeline\map;
-use function Pipeline\take;
 
 /**
  * @covers \Pipeline\Standard
@@ -194,98 +192,5 @@ final class SelectTest extends TestCase
             ->willReturn($pipeline);
 
         $pipeline->filter($func, $strict);
-    }
-
-    public function testSelectOnRejectCallback(): void
-    {
-        $rejected = [];
-
-        $pipeline = fromValues(1, 2, 3, 4, 5);
-        $pipeline->select(
-            fn($value) => 0 === $value % 2,
-            onReject: function ($value) use (&$rejected) {
-                $rejected[] = $value;
-            },
-        );
-
-        $this->assertSame([2, 4], $pipeline->toList());
-        $this->assertSame([1, 3, 5], $rejected);
-    }
-
-    public function testSelectOnRejectCallbackWithKey(): void
-    {
-        $rejected = [];
-
-        $pipeline = take(new ArrayIterator(['a' => 1, 'b' => 2, 'c' => 3]));
-        $pipeline->select(
-            fn($value) => 2 === $value,
-            onReject: function ($value, $key) use (&$rejected) {
-                $rejected[$key] = $value;
-            },
-        );
-
-        $this->assertSame(['b' => 2], $pipeline->toAssoc());
-        $this->assertSame(['a' => 1, 'c' => 3], $rejected);
-    }
-
-    public function testSelectOnRejectCallbackWithArrayInput(): void
-    {
-        $rejected = [];
-
-        $pipeline = fromArray([1, 2, 3]);
-        $pipeline->select(
-            fn($value) => $value > 2,
-            onReject: function ($value) use (&$rejected) {
-                $rejected[] = $value;
-            },
-        );
-
-        $this->assertSame([3], $pipeline->toList());
-        $this->assertSame([1, 2], $rejected);
-    }
-
-    public function testSelectOnRejectCallbackWithStrictMode(): void
-    {
-        $rejected = [];
-
-        $pipeline = fromValues(null, false, 0, '', 'valid');
-        $pipeline->select(
-            onReject: function ($value) use (&$rejected) {
-                $rejected[] = $value;
-            },
-        );
-
-        $this->assertSame([0, '', 'valid'], $pipeline->toList());
-        $this->assertSame([null, false], $rejected);
-    }
-
-    public function testSelectOnRejectCallbackWithNonStrictMode(): void
-    {
-        $rejected = [];
-
-        $pipeline = fromValues(null, false, 0, '', 'valid');
-        $pipeline->select(
-            strict: false,
-            onReject: function ($value) use (&$rejected) {
-                $rejected[] = $value;
-            },
-        );
-
-        $this->assertSame(['valid'], $pipeline->toList());
-        $this->assertSame([null, false, 0, ''], $rejected);
-    }
-
-    public function testSelectOnRejectCallbackWithInternalCallable(): void
-    {
-        $queue = new SplQueue();
-
-        $pipeline = fromValues(1, 2, 3);
-        $pipeline->select(
-            fn($value) => 2 === $value,
-            onReject: $queue->enqueue(...),
-        );
-
-        $this->assertSame([2], $pipeline->toList());
-        $this->assertSame([1, 3], iterator_to_array($queue));
     }
 }
